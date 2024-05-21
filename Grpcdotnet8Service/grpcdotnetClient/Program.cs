7d5ab36fa92063d6be3e07d7694feba12c429428
unknown <vadim_makarenkov@mail>
2024-05-21 16:54:55 +0100

    initial upload

---
+++ Grpcdotnet8Service/grpcdotnetClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Grpcdotnet8Service;
+using System.IO.IsolatedStorage;
+using System.Text;
+
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7215", new GrpcChannelOptions
+{
+    HttpHandler = new WinHttpHandler()
+});
+
+
+var client = new Greeter.GreeterClient(channel);
+
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+Console.Write("name: ");
+var name = Console.ReadLine();
+var watch = System.Diagnostics.Stopwatch.StartNew();
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+Console.WriteLine($"server reply: {reply.Message}");
+watch.Stop();
+var elapsedMs = watch.ElapsedMilliseconds;
+Console.WriteLine($"{elapsedMs} ms taken");
+
+Console.ReadKey();
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # grpcdotnet
+
+
+## if you need venv
+```
+python -m venv c:\Users\User\venv
+
+c:\Users\User\venv\Scripts\activate
+
+upgrade pip python.exe -m pip install --upgrade pip
+
+pip install -r requirements.txt
+
+python async_greeter_server.py
+```
